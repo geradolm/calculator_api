08b1fc3f1ad9b4f5d54f27e908c3e172cbce6d45
geradolm <gerardo.marin@developyn.com>
2022-02-01 16:22:22 +0000

    Update

--- back/app.js
+++ back/app.js
@@ -1,35 +1,75 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const cors = require('cors');
-
+const cors = require("cors");
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
-app.get('/sum/:num1/:num2', function (req, res) {
-  const sum = (parseInt(req.params.num1) + parseInt(req.params.num2)).toString();
-  res.send(sum);
-})
+app.get("/:operator/:num1/:num2", function (req, res) {
+  let operation;
 
-app.get('/subtraction/:num1/:num2', function (req, res) {
-  const subtraction = (parseInt(req.params.num1) - parseInt(req.params.num2)).toString();
-  res.send(subtraction);
-})
+  switch (req.params.operator) {
+    case "sum":
+      operation = (
+        parseInt(req.params.num1) + parseInt(req.params.num2)
+      ).toString();
+      res.send(operation);
+      break;
+    case "subtraction":
+      operation = (
+        parseInt(req.params.num1) - parseInt(req.params.num2)
+      ).toString();
+      res.send(operation);
+      break;
+    case "multiplication":
+      operation = (
+        parseInt(req.params.num1) * parseInt(req.params.num2)
+      ).toString();
+      res.send(operation);
+      break;
+    case "division":
+      operation = (
+        parseInt(req.params.num1) / parseInt(req.params.num2)
+      ).toString();
+      res.send(operation);
+      break;
+    default:
+      res.send("Error");
+  }
+});
 
-app.get('/multiplication/:num1/:num2', function (req, res) {
-  const multiplication = (parseInt(req.params.num1) * parseInt(req.params.num2)).toString();
-  res.send(multiplication);
-})
+// app.get("/sum/:num1/:num2", function (req, res) {
+//   const sum = (
+//     parseInt(req.params.num1) + parseInt(req.params.num2)
+//   ).toString();
+//   res.send(sum);
+// });
 
-app.get('/division/:num1/:num2', function (req, res) { 
-  const division = (parseInt(req.params.num1) / parseInt(req.params.num2)).toString();
-  res.send(division);
-})
+// app.get("/subtraction/:num1/:num2", function (req, res) {
+//   const subtraction = (
+//     parseInt(req.params.num1) - parseInt(req.params.num2)
+//   ).toString();
+//   res.send(subtraction);
+// });
+
+// app.get("/multiplication/:num1/:num2", function (req, res) {
+//   const multiplication = (
+//     parseInt(req.params.num1) * parseInt(req.params.num2)
+//   ).toString();
+//   res.send(multiplication);
+// });
+
+// app.get("/division/:num1/:num2", function (req, res) {
+//   const division = (
+//     parseInt(req.params.num1) / parseInt(req.params.num2)
+//   ).toString();
+//   res.send(division);
+// });
